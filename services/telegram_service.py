ba27e6e8b4e3a9da7819f3a9b0b7d323ea3ac8c9
Wingbean <taeey.com@gmail.com>
2025-06-08 10:23:06 +0700

    C E telegram_service ดึงวันที่ออกมาจาก sql

--- services/telegram_service.py
+++ services/telegram_service.py
@@ -1,14 +1,37 @@
 import requests
 from config.settings import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
 from utils.logger import setup_logger
+import os
+from dotenv import load_dotenv
+import re
+
+load_dotenv()
 
 logger = setup_logger(__name__)
 
 def send_report_to_telegram(df):
+    filename = os.getenv("SQL_NAME")
+    base_dir = os.path.dirname(__file__)
+    sql_path = os.path.normpath(os.path.join(base_dir, "..", "sql", filename))
+
+    """หาวันที่ในSQL"""
+
+    # อ่านไฟล์ SQL มาเป็น string
+    with open(sql_path, "r", encoding="utf-8") as f:
+        sql_text = f.read()
+
+        # ใช้ regex หา pattern วันที่ รูปแบบ "YYYY-MM-DD"
+        date_pattern = r'"(\d{4}-\d{2}-\d{2})"'
+
+        match = re.search(date_pattern, sql_text)
+
+        date_str = match.group(1)
+    
     if df.empty:
         message = "ไม่พบข้อมูลผู้ป่วยที่เข้าเงื่อนไขในวันนี้"
     else:
-        intro = "HN ผู้ป่วยยังไม่มีวันลงทะเบียน\n"
+        
+        intro = f"HN ผู้ป่วยยังไม่มีวันลงทะเบียน\nประจำวันที่ {date_str} \n"
         df_text = df.to_markdown(index=False)
         message = intro + "\n" + df_text
 
