b59a5a03c667b281938a6e6205adbecc1c1a0513
Wingbean <taeey.com@gmail.com>
2025-06-09 12:01:39 +0700

    N edit date to telegram

--- services/telegram_service.py
+++ services/telegram_service.py
@@ -4,34 +4,23 @@ from utils.logger import setup_logger
 import os
 from dotenv import load_dotenv
 import re
+from datetime import date
+
 
 load_dotenv()
 
 logger = setup_logger(__name__)
 
 def send_report_to_telegram(df):
-    filename = os.getenv("SQL_NAME")
-    base_dir = os.path.dirname(__file__)
-    sql_path = os.path.normpath(os.path.join(base_dir, "..", "sql", filename))
-
-    """หาวันที่ในSQL"""
-
-    # อ่านไฟล์ SQL มาเป็น string
-    with open(sql_path, "r", encoding="utf-8") as f:
-        sql_text = f.read()
-
-        # ใช้ regex หา pattern วันที่ รูปแบบ "YYYY-MM-DD"
-        date_pattern = r'"(\d{4}-\d{2}-\d{2})"'
-
-        match = re.search(date_pattern, sql_text)
-
-        date_str = match.group(1)
     
+    date_str = date.today().isoformat()
+
     if df.empty:
-        message = "ไม่พบข้อมูลผู้ป่วยที่เข้าเงื่อนไขในวันนี้"
+        intro = f"รายงาน HN ผู้ป่วยยังไม่มีวันลงทะเบียน\nประจำวันที่ {date_str} \n"
+        message = intro + "--ไม่พบข้อมูลผู้ป่วยที่เข้าเงื่อนไขในวันนี้--"
     else:
         
-        intro = f"HN ผู้ป่วยยังไม่มีวันลงทะเบียน\nประจำวันที่ {date_str} \n"
+        intro = f"รายงาน HN ผู้ป่วยยังไม่มีวันลงทะเบียน\nประจำวันที่ {date_str} \n"
         df_text = df.to_markdown(index=False)
         message = intro + "\n" + df_text
 
--- sql/noregisdate.sql
+++ sql/noregisdate.sql
@@ -2,6 +2,6 @@ SELECT o.hn,
         MAX(c.regdate)
 FROM ovst o
 LEFT OUTER JOIN clinicmember c ON o.hn = c.hn
-WHERE o.vstdate = "2025-06-09" AND o.main_dep = '033' AND c.regdate IS NULL
+WHERE o.vstdate = CURDATE() AND o.main_dep = '033' AND c.regdate IS NULL
 GROUP BY o.hn
 ;
