495685ca772129fcdc7594e294ce34f2ec66192c
Wingbean <taeey.com@gmail.com>
2025-06-13 08:54:04 +0700

    ok

--- sql/count_pt_dep.sql
+++ sql/count_pt_dep.sql
@@ -1,6 +1,11 @@
 SELECT 
 	o.main_dep AS "รหัส"
-	,k.department AS "แผนก"
+	,CASE
+		WHEN k.department = "คลินิกวัณโรค/ฝ่ายสุขาภิบาลฯ" THEN "ห้องเบอร์ 9"
+		WHEN k.department = "จุดซักประวัติ" THEN "OPD"
+		WHEN k.department = "คลินิกดอกลำดวน" THEN "NCD"
+		ELSE k.department
+	END AS "DeptName"
 	,COUNT(vn) AS "จำนวน"
 FROM ovst o 
 LEFT OUTER JOIN kskdepartment k ON k.depcode = o.main_dep 
