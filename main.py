e6a0dbb918b8c5a080dbd86e0fab680d57b86b1a
Wingbean <taeey.com@gmail.com>
2025-07-17 15:03:14 +0700

    copy back from pi before to docker

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     df1 = fetch_noregisdate()
     #==(df, title, chat_group_name)
 
-    send_report_to_telegram(df1, "รายงาน HN ไม่มีวันลงทะเบียน", "test")
+    send_report_to_telegram(df1, "รายงาน HN ไม่มีวันลงทะเบียน", "NCD")
     #send_report_to_line(df1, "รายงาน HN ผู้ป่วยยังไม่มีวันลงทะเบียน", "me")
     #send_dataframe_as_line_flex(df1, "รายงาน HN ไม่มีวันลงทะเบียน", "me")
     #==================================#
@@ -15,18 +15,18 @@ def main():
     df2 = fetch_count_pt_dep()
     #==(df, title, chat_group_name)
 
-    send_report_to_telegram(df2, "รายงาน ผู้มาใช้บริการแยกแผนก", "test")
-    send_dataframe_as_image(df2, "รายงาน ผู้มาใช้บริการแยกแผนก", "test")
+    #send_report_to_telegram(df2, "รายงาน ผู้มาใช้บริการแยกแผนก", "test")
+    send_dataframe_as_image(df2, "รายงาน ผู้มาใช้บริการแยกแผนก", "NCD")
     #send_report_to_line(df2, "รายงาน ผู้มาใช้บริการแยกแผนก", "me")
     #send_dataframe_as_line_flex(df2, "รายงาน ผู้มาใช้บริการแยกแผนก", "me")
 
     df3 = fetch_count_admit()
-    send_report_to_telegram(df3, "จำนวนผู้ป่วย Admit", "test")
-    send_dataframe_as_image(df3, "จำนวนผู้ป่วย Admit", "test")
+    #send_report_to_telegram(df3, "จำนวนผู้ป่วย Admit", "test")
+    send_dataframe_as_image(df3, "จำนวนผู้ป่วย Admit", "NCD")
 
     df4 = fetch_ward_status()
-    send_report_to_telegram(df4, "สถานะเตียงใน ward", "test")
-    send_dataframe_as_image(df4, "สถานะเตียงใน ward", "test")
+    #send_report_to_telegram(df4, "สถานะเตียงใน ward", "test")
+    send_dataframe_as_image(df4, "สถานะเตียงใน ward", "NCD")
 
 if __name__ == "__main__":
     main()
---
+++ main2.py
@@ -0,0 +1,32 @@
+from services.data_service import fetch_noregisdate, fetch_count_pt_dep, fetch_ward_status, fetch_count_admit
+from services.telegram_service import send_report_to_telegram
+from services.telegram_service import send_dataframe_as_image
+from services.line_service import send_report_to_line, send_dataframe_as_line_flex
+
+def main():
+    #df1 = fetch_noregisdate()
+    #==(df, title, chat_group_name)
+
+    #send_report_to_telegram(df1, "รายงาน HN ไม่มีวันลงทะเบียน", "NCD")
+    #send_report_to_line(df1, "รายงาน HN ผู้ป่วยยังไม่มีวันลงทะเบียน", "me")
+    #send_dataframe_as_line_flex(df1, "รายงาน HN ไม่มีวันลงทะเบียน", "me")
+    #==================================#
+
+    df2 = fetch_count_pt_dep()
+    #==(df, title, chat_group_name)
+
+    #send_report_to_telegram(df2, "ผู้มาบริการแยกแผนก", "test")
+    send_dataframe_as_image(df2, "ผู้มาใช้บริการแยกแผนก", "NCD")
+    #send_report_to_line(df2, "ผู้มาใช้บริการแยกแผนก", "me")
+    #send_dataframe_as_line_flex(df2, "ผู้มาใช้บริการแยกแผนก", "me")
+
+    #df3 = fetch_count_admit()
+    #send_report_to_telegram(df3, "จำนวนผู้ป่วย Admit", "test")
+    #send_dataframe_as_image(df3, "จำนวนผู้ป่วย Admit", "NCD")
+
+    df4 = fetch_ward_status()
+    #send_report_to_telegram(df4, "สถานะเตียง", "test")
+    send_dataframe_as_image(df4, "สถานะเตียง", "NCD")
+
+if __name__ == "__main__":
+    main()
